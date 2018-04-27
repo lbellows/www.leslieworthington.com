4f8bb186d1633c9b81a008511c925a8fe6ae151a
lbellows <lbellows@gmail.com>
2018-04-27 16:59:34 -0400

    fix bug with serve, css, link, title

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,16 @@
 $(function(){
-  
   //query '../img/
   var imgData = '../imgData.json';
   var imageFolder = "../img/";
 
   //add images to dom
   $.get(imgData).then(function(data) {
-    var dataobj = JSON.parse(data);
-    dataobj.forEach(imgName => {
-      var imgSrc = imageFolder + imgName
-      var template = 
+    if(typeof data != "object")
+      data = JSON.parse(data);
+
+    data.forEach(imgName => {
+    var imgSrc = imageFolder + imgName
+    var template = 
 `
 <div class="col-md-4">
   <div class="card mb-4 box-shadow">
